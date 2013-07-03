11a37ea736f715ac763c0d895b11891a34098d31
simingcheng <siming.cheng@gmail.com>
2013-07-02 23:39:53 -0400

    Initialize Scala Repo.

---
+++ TimerFunction.scala
@@ -0,0 +1,18 @@
+object TimerFunction {
+    def oncePerSecond(callback: () => Unit) {
+        while(true) {
+            callback()
+            Thread sleep 1000
+        }
+    }
+
+    def timeFlies() {
+        println("time flies like an arrow...")
+    }
+
+    def main(args: Array[String]) {
+        //oncePerSecond(timeFlies);
+        oncePerSecond(() =>
+            println("time flies like an arrow..."));
+    }
+}
